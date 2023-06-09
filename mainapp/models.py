69ab6b19fd76d540dfd829f9701d2a9356419796
huseynovfuad06 <fuadhuseynov0606@gmail.com>
2023-06-09 21:17:37 +0400

    media part

--- mainapp/models.py
+++ mainapp/models.py
@@ -17,3 +17,14 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+def upload_to(instance, filename):
+    return f"products/{instance.product.name}/{filename}"
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=upload_to)
+
+    def __str__(self):
+        return self.product.name
