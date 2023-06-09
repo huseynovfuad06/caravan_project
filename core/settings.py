a1c3a523d86900cbb2b93563f27f44a551b139d9
huseynovfuad06 <fuadhuseynov0606@gmail.com>
2023-06-09 20:46:21 +0400

    static part

--- core/settings.py
+++ core/settings.py
@@ -122,8 +122,20 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+import os
 
-STATIC_URL = '/static/'
+STATIC_URL = '/myproject/static/'
+
+
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# STATIC_ROOT = BASE_DIR / "static"
+
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,11 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("mainapp/", include("mainapp.urls"))
-
-
-    # mainapp/index/
-    # mainapp/create/
 ]
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
